39641480dd58cded72ccc7a8d339a560a0e8284c
skalva <olivierlecornu@yahoo.fr>
2017-03-11 18:46:33 +0100

    Ajout des scripts BDD
    Ajout dans les sources des scripts SQL de creation et suppression de la
    BDD associés au projet

---
+++ Filmotheque/script_creation.sql
@@ -0,0 +1,67 @@
+﻿create database filmotheque;
+
+CREATE TABLE fichier
+(
+  id serial NOT NULL,
+  chemin character varying(500),
+  CONSTRAINT prk_constraint_fichier PRIMARY KEY (id),
+  CONSTRAINT unique_constraint_operation UNIQUE (chemin)
+);
+
+CREATE TABLE FILM
+(
+	id serial NOT NULL,
+	idTMDB Integer,
+	idFichier Integer,
+	titre varchar(255),
+	titreOriginal varchar(255),
+	resume varchar(5000),
+	affiche varchar(255),
+	cleVideo varchar(255),
+	siteVideo varchar(255),
+	popularite float,
+	note float,
+	CONSTRAINT prk_constraint_film PRIMARY KEY (id),	
+	CONSTRAINT fk_film_idFichier FOREIGN KEY (idFichier)
+		REFERENCES fichier(id) 
+);
+
+
+
+CREATE TABLE SERIE
+(
+	id serial NOT NULL,
+	idTMDB Integer,	
+	titre varchar(255),
+	titreOriginal varchar(255),
+	resume varchar(5000),
+	affiche varchar(255),
+	popularite float,
+	note float,
+	cleVideo varchar(255),
+	siteVideo varchar(255),
+	CONSTRAINT prk_constraint_serie PRIMARY KEY (id),
+	CONSTRAINT unique_constraint_serie UNIQUE (idTMDB)	
+ 
+);
+
+CREATE TABLE SAISON
+(
+	id serial NOT NULL,
+	idSerie integer,		
+	resume varchar(5000),
+	affiche varchar(255),
+	dateSortie varchar(255),
+	numero Integer,
+	CONSTRAINT prk_constraint_SAISON PRIMARY KEY (id),
+	CONSTRAINT unique_constraint_saison UNIQUE (idSerie,numero),
+	CONSTRAINT fk_film_idSerie FOREIGN KEY (idSerie)
+		REFERENCES SERIE(id) 
+ 
+);
+
+
+
+
+
+
---
+++ Filmotheque/script_suppression.sql
@@ -0,0 +1,11 @@
+delete from film;
+delete from fichier;
+delete from saison;
+delete from serie;
+
+drop table film;
+drop table fichier;
+drop table saison;
+drop table serie;
+
+drop database filmotheque;
